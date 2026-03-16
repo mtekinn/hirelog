0713b14b21982330ce9e0432a2aa6b8f3ffcf67b
mtekinn <tekinmdev@gmail.com>
2026-03-16 13:01:22 +0300

    fix: remove SpringBootTest from context test

--- src/test/java/com/example/hirelog/HirelogApplicationTests.java
+++ src/test/java/com/example/hirelog/HirelogApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.hirelog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HirelogApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
